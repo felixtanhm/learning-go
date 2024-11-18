89b00996b793559b08af8e1755705160554bfdba
felixtanhm <felixtanhm@gmail.com>
2024-11-18 17:59:37 +0800

    arrays & slices

--- learnGoWithTests/arrays-slices/sum.go
+++ learnGoWithTests/arrays-slices/sum.go
@@ -7,3 +7,23 @@ func Sum(nums []int) (result int) {
 	}
 	return result
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, values := range numbersToSum {
+		sums = append(sums, Sum(values))
+	}
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, values := range numbersToSum {
+		if len(values) == 0 {
+			sums = append(sums, 0)
+		} else {
+			sums = append(sums, Sum(values[1:]))
+		}
+	}
+	return sums
+}
--- learnGoWithTests/arrays-slices/sum_test.go
+++ learnGoWithTests/arrays-slices/sum_test.go
@@ -2,6 +2,7 @@ package arraysslices
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 )
 
@@ -29,5 +30,23 @@ func BenchmarkSum(b *testing.B) {
 	nums := []int{1, 2, 3, 4, 5}
 	for i := 0; i < b.N; i++ {
 		Sum(nums)
+	}
+}
+
+func TestSumAll(t *testing.T) {
+	result := SumAll([]int{1, 2, 3}, []int{3, 3})
+	expected := []int{6, 6}
+
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Result: %v | Expected: %v", result, expected)
+	}
+}
+
+func TestSumAllTails(t *testing.T) {
+	result := SumAllTails([]int{0, 2}, []int{2, 9})
+	expected := []int{2, 9}
+
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Result: %v | Expected: %v", result, expected)
 	}
 }
